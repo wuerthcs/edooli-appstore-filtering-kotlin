5f658e4a2a271451aada2bdbb2869bd92302480e
Falco Bartsch <falcobartsch@gmail.com>
2022-07-20 11:08:23 +0200

    added relaxed-query-vars to application.properties

--- src/main/kotlin/com/wuerthcs/platform/filtering/controller/FilteringController.kt
+++ src/main/kotlin/com/wuerthcs/platform/filtering/controller/FilteringController.kt
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import javax.persistence.AttributeOverride
 
 @RestController
 @RequestMapping("/api/v1")
